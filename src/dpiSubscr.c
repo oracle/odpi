27e76da0376874918d6237a2ebfd452256e2418f
Anthony Tuininga <anthony.tuininga@oracle.com>
2019-02-07 07:20:14 -0700

    Correct code for freeing CQN message objects (https://github.com/oracle/odpi/issues/96).

--- src/dpiSubscr.c
+++ src/dpiSubscr.c
@@ -275,8 +275,8 @@ static void dpiSubscr__freeMessage(dpiSubscrMessage *message)
             query = &message->queries[i];
             if (query->numTables > 0) {
                 for (j = 0; j < query->numTables; j++) {
-                    if (query->tables[i].numRows > 0)
-                        dpiUtils__freeMemory(query->tables[i].rows);
+                    if (query->tables[j].numRows > 0)
+                        dpiUtils__freeMemory(query->tables[j].rows);
                 }
                 dpiUtils__freeMemory(query->tables);
             }
