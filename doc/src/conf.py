a1c8f55fe3537d457eaa46d66c78ca016b858e35
Anthony Tuininga <anthony.tuininga@oracle.com>
2019-03-07 09:30:40 -0700

    Bump version in preparation for a patch release.

--- doc/src/conf.py
+++ doc/src/conf.py
@@ -33,7 +33,7 @@ author = 'Oracle'
 version = '3.1'
 
 # the full version, including alpha/beta/rc tags
-release = '3.1.2'
+release = '3.1.3'
 
 # the theme to use for HTML pages
 html_theme = 'oracle'
