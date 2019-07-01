34da7033238754efc64ffbc7f006b5ba47fc6e9b
Anthony Tuininga <anthony.tuininga@oracle.com>
2019-07-01 16:53:56 -0600

    Removing -dev designation in preparation for release of ODPI-C 3.2.0.

--- doc/src/conf.py
+++ doc/src/conf.py
@@ -33,7 +33,7 @@ author = 'Oracle'
 version = '3.2'
 
 # the full version, including alpha/beta/rc tags
-release = '3.2.0-dev'
+release = '3.2.0'
 
 # the theme to use for HTML pages
 html_theme = 'oracle'
