21b1888f6ae1357d039fdbc4882fd537dd1c6130
Anthony Tuininga <anthony.tuininga@oracle.com>
2019-08-12 20:40:30 -0600

    Preparing to release ODPI-C 3.2.1.

--- doc/src/conf.py
+++ doc/src/conf.py
@@ -33,7 +33,7 @@ author = 'Oracle'
 version = '3.2'
 
 # the full version, including alpha/beta/rc tags
-release = '3.2.0'
+release = '3.2.1'
 
 # the theme to use for HTML pages
 html_theme = 'oracle'
