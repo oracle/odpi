0ca3af9c4289e8527c5515721614dd56eef11f95
Anthony Tuininga <anthony.tuininga@gmail.com>
2021-12-16 13:51:30 -0700

    Bump version in preparation for making new changes.

--- doc/src/conf.py
+++ doc/src/conf.py
@@ -30,10 +30,10 @@ author = 'Oracle'
 # |release|, also used in various other places throughout the built documents
 #
 # the short X.Y version
-version = '4.3'
+version = '4.4'
 
 # the full version, including alpha/beta/rc tags
-release = '4.3.0'
+release = '4.4.0-dev'
 
 # the theme to use for HTML pages
 html_theme = 'oracle'
